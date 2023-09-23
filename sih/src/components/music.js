92754abbb396adfdeca099e66801450af43894ea
sharvithaa <dharmalingamrandd@gamil.com>
2023-09-23 11:19:29 +0530

    updated

--- sih/src/components/music.js
+++ sih/src/components/music.js
@@ -4,16 +4,15 @@ const music=()=>{
     return(
    <div className="pongal">
     <h1 className="text-center text-4xl m-6 font-bold">Thiruvaiyaru Festival</h1>
-    <div className="shadow-xl shadow-gray-400">
-    <div className="flex items-center justify-center ">
-    <img src="https://www.outlookindia.com/outlooktraveller/public/uploads/files/2015/02/051015153810-2012-9.jpg" className="m-10 pl-0 h-[400px] w-[500px] "></img>
-    <p className="text-justify pr-6">Thyagaraja was the greatest among the music composers of South India and one of the musical prodigies of all time. He was the father of modern Karnataka music and his works are of delicate spirituality, full of melodic beauty and in the highest sense artistic. As a child he as already remarkable for his predilection for music and a gift for composing and wrote his first song when he was hardly 13. Soon he reached the loftiest pinnacle of musical fames and composed the grandest songs ever written and two music plays of uneqalled beauty.
+    <h1 className="text-center text-xl font-bold text-gray-300">Jan 2024</h1>
+    <img src="https://www.outlookindia.com/outlooktraveller/public/uploads/files/2015/02/051015153810-2012-9.jpg" className="p-2 h-96 w-screen"></img>
+    <div className="m-2 p-2">    
+    <p className="text-justify p-4">Thyagaraja was the greatest among the music composers of South India and one of the musical prodigies of all time. He was the father of modern Karnataka music and his works are of delicate spirituality, full of melodic beauty and in the highest sense artistic. As a child he as already remarkable for his predilection for music and a gift for composing and wrote his first song when he was hardly 13. Soon he reached the loftiest pinnacle of musical fames and composed the grandest songs ever written and two music plays of uneqalled beauty.
     The Thiruvaiyaru festival goes by the name of Thiruvaiyaru Thyagaraja Music Festival. This festival is regarded as one of the biggest music festivals in India. It is a five-day-long music festival, dedicated to Saint Thyagaraja who was the greatest saint composer of Carnatic music.</p>
   </div>
-  <button className="bg-blue-500 hover:bg-blue-300 p-2 rounded-full">Book Tickets</button>
+  <button className="bg-blue-500 hover:bg-blue-300 m-2 p-2 rounded-3xl text-center mx-auto block">Book Tickets</button>
   </div>
-  
-</div>
+
     )
 }
 export default music;
